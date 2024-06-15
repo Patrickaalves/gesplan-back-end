54abe046387641f2847d0b24054eb9fd3288e929
Patrickaalves <patrickalvessc@gmail.com>
2024-06-15 19:33:49 -0300

    Configurando o cors na aplicação

---
+++ src/main/java/com/patrick/gesplanbakend/configuration/WebConfigCors.java
@@ -0,0 +1,15 @@
+package com.patrick.gesplanbakend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigCors implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5174")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
