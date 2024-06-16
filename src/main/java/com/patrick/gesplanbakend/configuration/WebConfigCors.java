8120e6d70308a1315aad05e60d96299da29aa3e4
Patrickaalves <patrickalvessc@gmail.com>
2024-06-16 20:27:58 -0300

    Adicionando comentarios para melhorar o entendimento da classe

--- src/main/java/com/patrick/gesplanbakend/configuration/WebConfigCors.java
+++ src/main/java/com/patrick/gesplanbakend/configuration/WebConfigCors.java
@@ -6,6 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfigCors implements WebMvcConfigurer {
+    /***
+     * Classe criada para resolver o erro "blocked by CORS policy: Cross origin requests are only supported for
+     * protocol schemes: http, data, chrome-extension, edge, https, chrome-untrusted" ou derivados
+     * ***/
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
